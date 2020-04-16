7e1514dc2b1a2fe0e500844210857d3cdc9972af
helloclient.business <helloclient.business@gmail.com>
2020-04-16 21:42:30 +0530

    Initial commit. created spring boot project and configure the application properties

---
+++ hcbookstore/src/main/java/in/helloclient/hcbookstore/HcbookstoreApplication.java
@@ -0,0 +1,13 @@
+package in.helloclient.hcbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcbookstoreApplication.class, args);
+	}
+
+}
---
+++ hcbookstore/src/test/java/in/helloclient/hcbookstore/HcbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.helloclient.hcbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
